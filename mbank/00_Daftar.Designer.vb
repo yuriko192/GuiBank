94b5ecb5fed40aaef82f0eec1f5e2ff30a282656
NATANAEL <natanaelsutanto@gmail.com>
2019-11-05 11:45:40 +0700

    ada button

--- mbank/00_Daftar.Designer.vb
+++ mbank/00_Daftar.Designer.vb
@@ -22,17 +22,30 @@ Partial Class _00_Daftar
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.SuspendLayout
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         '_00_Daftar
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.YellowGreen
         Me.ClientSize = New System.Drawing.Size(305, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "_00_Daftar"
         Me.Text = "_00_Daftar"
         Me.ResumeLayout(false)
 
 End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
